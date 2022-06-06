1f7f9d494c90f83adb99f726a24998c3e1d1261d
Eknath Salvi <salvieknath18@gmail.com>
2022-06-07 00:35:19 +0530

    circleci setup3

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "eknath-terraform-backend"
-    key    = "terraform_test/dev.tfstate"
+    key    = "terraform_test/qa.tfstate"
     region = "ap-south-1"
   }
 }
